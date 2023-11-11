54bc122312d6ddd3cb7305f4d86efb8bcf86dacc
natiapreutesei <natiapreutesei@gmail.com>
2023-11-11 14:06:28 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,30 @@ import '../scss/mycss.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var masonryContainer = document.querySelector('.masonry-container');
+
+    // Hide images initially to prevent layout shifts
+    masonryContainer.querySelectorAll('img').forEach(img => {
+        img.style.display = 'none';
+    });
+
+    // Use imagesLoaded library to wait for all images to be loaded
+    imagesLoaded(masonryContainer, function () {
+        // Show images
+        masonryContainer.querySelectorAll('img').forEach(img => {
+            img.style.display = 'block';
+        });
+
+        // Initialize Masonry after images are loaded
+        var masonry = new Masonry(masonryContainer, {
+            itemSelector: '.masonry-item',
+            // Other options...
+        });
+    });
+});
